28cd7a0a3a49149363c8c5c15df90c1f2654bfa3
thebladehit <boqdanyarmolka@gmail.com>
2025-06-11 23:23:52 +0300

    Delete Source field where it's not required

--- libs/common/src/utils/map-event-to-prisma.ts
+++ libs/common/src/utils/map-event-to-prisma.ts
@@ -2,7 +2,6 @@ import { Event } from '@app/common';
 import {
   FacebookEventType,
   FunnelStage,
-  Source,
   TiktokEventType,
 } from '@prisma/client';
 import { convertToPrismaEventType } from '@app/common/utils';
@@ -15,7 +14,6 @@ export const mapEventToPrismaType = <
   return {
     id: event.eventId,
     timestamp: event.timestamp,
-    source: event.source.toUpperCase() as unknown as Source,
     funnelStage: event.funnelStage.toUpperCase() as unknown as FunnelStage,
     eventType: convertToPrismaEventType<T>(event.eventType),
     userId: event.data.user.userId,
